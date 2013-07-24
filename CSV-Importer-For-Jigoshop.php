504562e3550712a1cae872b0939c7ef390c43d77
mag_oberon <mag_oberon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-24 21:48:40 +0000

    version 1.0 release
    git-svn-id: https://plugins.svn.wordpress.org/jigoshop-csv-importer/trunk@745919 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ CSV-Importer-For-Jigoshop.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Plugin Name: Jigoshop CSV Importer
+Description: CSV Importer for Jigoshop
+Version: 1.0
+Author: promag
+Author URI: http://bloggerhelp24.blogspot.com/
+License: GPL
+parent: jigoshop
+*/
+
+/**
+ * This file is part of jigoshop-csv-importer.
+ * 
+ * Simple woocommerce CSV Importer is Pro software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Pro Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * simple woocommerce-csv-importer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with simple-wocommerce-csv-importer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class CSVImporterForJigoshop {
+    function __construct() {
+        add_action( 'init', array( $this, 'init' ) );
+        if ( is_admin() ) {
+            add_action( 'admin_menu', array( $this, 'admin_menu' ), 99 );
+        }
+    }
+
+    function init() {
+        if ( function_exists( 'import_plugin_textdomain' ) )
+            import_plugin_textdomain( 'tcp_csvl', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+    }
+
+    function admin_menu() {
+        
+if (function_exists('add_menu_page'))
+{
+add_menu_page('Jigoshop CSV Importer', 'Jigoshop CSV Importer', 'administrator', dirname( __FILE__ ) . '/admin/Jigoshop-CSV-Importer.php');
+}
+    }
+}
+
+new CSVImporterForJigoshop();
+?>
---
+++ readme.txt
@@ -0,0 +1,54 @@
+=== Jigoshop CSV Importer ===
+Contributors: mag_oberon
+Donate link: http://bloggerhelp24.blogspot.com/p/blog-page_6415.html
+Tags: jigoshop, jigoshop csv, jigoshop csv import, csv, import, plugin 
+Requires at least: 2.8+
+Tested up to: 3.5.2
+Stable tag:trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Import CSV products into Jigoshop.
+
+== Description ==
+
+Import Products into your Jigoshop store with the Jigoshop CSV Importer. CSV Importer lets you import all types of products, even product variations!
+
+= Jigoshop CSV Importer =
+
+* A simple, user friendly CSV product importer for Jigoshop.
+* Import hundreds or thousands of products at once. (The only limit is your patience!)
+* Import images via URL or local file path. (Now detects and skips duplicate images!)
+* Import hierarchical category structures. 
+* Import Tags
+* Import Description
+* Import Price
+* Import SKU
+* Import Thumbnail
+
+== Installation ==
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `<?php do_action('plugin_name_hook'); ?>` in your templates
+
+== Frequently asked questions ==
+
+= A question that someone might have =
+
+An answer to that question.
+
+== Screenshots ==
+
+1. 
+
+== Changelog ==
+
+
+
+== Upgrade notice ==
+
+
+
+== Arbitrary section 1 ==
+
